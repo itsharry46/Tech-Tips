41fcaecbeec0d6515e868d24fe47de85154cf887
Hariharan Achary <hariachary7@gmail.com>
2020-09-13 23:05:33 +0530

    home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Training from './components/Training';
 import './index.css';
 const Home = () => {
   return (
     <>
-      <div className="green-bg"></div>
-      <Navbar />      
+      <div className="green-bg">
+      <h1>The future of students is here, experience better</h1>
+        <p>Tech-Tips help E-lerning platform and students to improve there skills and engage more students.</p>
+      </div>
+      <Navbar />   
+      <Training />
     </>
   );
 };
---
+++ src/components/Training.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../index.css';
+
+const Training = (props) => {
+    return (
+        <>
+            <h1>Free-Trainings</h1>
+            <p>Learn New-Age Skills On The Go.</p>
+            
+
+        </>
+    );
+}
+
+export default Training;
