788e1d40211389da8eba0f64d2ff893d38f1ed92
Hariharan Achary <hariachary7@gmail.com>
2020-09-16 22:01:37 +0530

    login page

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import "./login.css";
-import leftimg from "../../images/landing-page.svg";
+import log from "./login.jpg";
 
 const Login = () => {
   return (
     <>
-      <div className="bg-color">
-        <div className="container">
-          <div className="card-login">
-            <div className="row">
-              <div className="col left-side-login">
-                <img src={leftimg} alt="left-image" />
-              </div>
-              <div className="col right-side-login">
-                
-              </div>
-            </div>
+      <div className="container-fluid login-bg">
+        <div className="row">
+          <div className="col login-left">
+            <img src={log} alt="login-left-img" />
           </div>
+
+
+          <div className="col login-right">
+            <h1>Login To Continue</h1>
+            <form className="login-form">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email</label>
+                <input type="email" required="true" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" required="true" class="form-control" id="exampleInputPassword1" />
+              </div>
+              <button type="submit" class="login-button">Login</button>
+            </form>
+          </div>
+
         </div>
       </div>
     </>
